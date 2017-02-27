67a91bd8b98a976b0f97f0aa8a4c933324840094
cduong13 <chrisduong83@gmail.com>
2017-02-27 15:12:17 +0700

    installed_packages for Ubuntu

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -61,16 +61,14 @@ describe 'ark::default' do
       { platform: 'ubuntu', platform_family: 'debian', version: '14.04' }
     end
 
+    let(:installed_packages) do
+      %w(libtool autoconf unzip rsync make gcc autogen shtool pkg-config)
+    end
+
     it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('make')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('autogen')
-      expect(chef_run).to install_package('shtool')
-      expect(chef_run).to install_package('pkg-config')
+      installed_packages.each do |name|
+        expect(chef_run).to install_package(name)
+      end
     end
 
     it "apache mirror" do
